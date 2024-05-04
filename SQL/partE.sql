3b9fb0a88b5b7630c86678331915f0dc30f8059d
idir6380 <idirbellili15@gmail.com>
2024-05-04 17:17:02 +0100

    SQL

--- SQL/partE.sql
+++ SQL/partE.sql
@@ -19,14 +19,33 @@ AND EXTRACT(YEAR FROM value(op).dateOp) BETWEEN 2000 AND 2022);
 --12    
 SELECT SUM(deref(VALUE(op)).montantOp)
 FROM COMPTE C, TABLE(C.OPERATIONS) op
-WHERE numCompte = 1180005564
-AND EXTRACT(YEAR FROM deref(VALUE(op)).dateOp) = 2023
-AND deref(VALUE(op)).natureOp = 'Credit';
-
-
-
-SELECT numCompte
-FROM COMPTE C, TABLE(C.OPERATIONS) op
 WHERE numCompte = 3023540212
 AND EXTRACT(YEAR FROM deref(VALUE(op)).dateOp) = 2023
 AND deref(VALUE(op)).natureOp = 'Credit';
+
+
+--13
+SELECT p.numPret,
+       deref(deref(p.compte).agence).numAgence AS numAgence,
+       deref(p.compte).numCompte AS numCompte,
+       deref(deref(p.compte).client).numClient AS numClient,
+       p.montantPret
+FROM Pret p
+WHERE p.dateEffet + p.duree > TRUNC(SYSDATE);
+
+
+
+SELECT p.numPret,
+       deref(deref(p.compte).agence).numAgence AS numAgence,
+       deref(p.compte).numCompte AS numCompte,
+       deref(deref(p.compte).client).numClient AS numClient,
+       p.montantPret - COALESCE((SELECT SUM(op.montantOp)
+                                 FROM Operation op
+                                 WHERE op.natureOp = 'Debit'
+                                   AND op.compte = p.compte), 0) AS montantRestant
+FROM Pret p
+WHERE (p.dateEffet + p.duree <= TRUNC(SYSDATE)) -- Vérifier si la période de remboursement est terminée
+      AND (p.montantPret - COALESCE((SELECT SUM(op.montantOp)
+                                     FROM Operation op
+                                     WHERE op.natureOp = 'Debit'
+                                       AND op.compte = p.compte), 0)) > 0; -- Vérifier si le montant restant dû est supérieur à zéro
